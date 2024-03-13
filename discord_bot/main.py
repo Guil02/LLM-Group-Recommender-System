275339126a0e4d849891e197e4fe25f094ecdad8
guil <g.bams@student.maastrichtuniversity.nl>
2024-03-13 12:43:33 +0100

    logging the bot's response

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -31,6 +31,7 @@ async def send_message(message: Message, user_message: str) -> None:
 
     try:
         response: str = get_response(user_message)
+        logging.info(response)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
