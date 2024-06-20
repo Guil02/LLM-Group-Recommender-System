526fb380df96cb4a73f5674e611d7433543f2d21
alexanderleonidas <alexanderleonidas@me.com>
2024-06-20 16:49:36 +0200

    moved traditional aggregation functions into grsmodule, and computed similarity between users in groups.

--- grsmodel/aggregationbot/aggregation_module.py
+++ grsmodel/aggregationbot/aggregation_module.py
@@ -1,6 +1,8 @@
 from grsmodel.main_runner.grs_module import GrsModule
 from discord import Client
 
+MAX_TAGS = 20
+MIN_SCORE = 3.0
 
 class AggregationModule(GrsModule):
 
@@ -9,3 +11,51 @@ class AggregationModule(GrsModule):
 
     def execute_module(self, bot: Client, *args, **kwargs):
         pass
+
+    def average_recommendation(self, preference_list, max_tags=MAX_TAGS):
+        item_totals = {}
+        item_counts = {}
+
+        for preferences in preference_list:
+            for item, score in preferences.items():
+                if item in item_totals:
+                    item_totals[item] += score
+                    item_counts[item] += 1
+                else:
+                    item_totals[item] = score
+                    item_counts[item] = 1
+
+        average_scores = {item: item_totals[item] / item_counts[item] for item in item_totals if item_totals[item] / item_counts[item] >= MIN_SCORE}
+        recommended_items = sorted(average_scores.items(), key=lambda x: x[1], reverse=True)
+        recommended_items = recommended_items[0:max_tags]
+        return recommended_items
+    
+    def least_misery_recommendation(self, preference_list, max_tags=MAX_TAGS):
+        item_min_scores = {}
+
+        for preferences in preference_list:
+            for item, score in preferences.items():
+                if item in item_min_scores:
+                    item_min_scores[item] = min(item_min_scores[item], score)
+                else:
+                    item_min_scores[item] = score
+
+        recommended_items = {item: score for item, score in item_min_scores.items() if score >= MIN_SCORE}
+        recommended_items=  sorted(recommended_items.items(), key=lambda x: x[1], reverse=True)[0:max_tags]
+
+        return recommended_items
+
+    def most_pleasure_recommendation(self, preference_list, max_tags=MAX_TAGS):
+        item_max_scores = {}
+
+        for preferences in preference_list:
+            for item, score in preferences.items():
+                if item in item_max_scores:
+                    item_max_scores[item] = max(item_max_scores[item], score)
+                else:
+                    item_max_scores[item] = score
+
+        recommended_items = {item: score for item, score in item_max_scores.items() if score >= MIN_SCORE}
+        recommended_items = sorted(recommended_items.items(), key=lambda x: x[1], reverse=True)[0:max_tags]
+
+        return recommended_items
