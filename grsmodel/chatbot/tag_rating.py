99da8b917748a207f6e79efee18d5c32a097f134
Lorenzo <lorenzopompigna1@gmail.com>
2024-06-26 13:14:36 +0100

    fixed recommendation

--- grsmodel/chatbot/tag_rating.py
+++ grsmodel/chatbot/tag_rating.py
@@ -87,8 +87,8 @@ class TagRating(View):
 
         response_lines = []
         for user_id, rating in ratings.items():
-            rating_emojis.get(rating, '❓')  # Default to question mark if rating is not 1-5
-            response_lines.append(rating_emojis)
+            rating = rating_emojis.get(rating, '❓')     # Default to question mark if rating is not 1-5
+            response_lines.append(rating)
 
         return f"{tag}:             {''.join(response_lines)}"
 
